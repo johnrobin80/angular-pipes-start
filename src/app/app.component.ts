dc3c59cd82a4d80cdae76abc408a8f258cfd73fa
robin-john-menaa <robinprojectgcp@gmail.com>
2022-02-09 12:32:43 +0400

    Pipes : Custom Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("stable");
+    }, 2000);
+  });
   servers = [
     {
       instanceType: "medium",
@@ -45,5 +50,14 @@ export class AppComponent {
       "list-group-item-warning": server.status === "offline",
       "list-group-item-danger": server.status === "critical",
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: "small",
+      name: "Robin Server",
+      status: "stable",
+      started: new Date(15, 1, 2022),
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ShortenPipe } from "./shorten.pipe";
 import { FilterPipe } from "./filter.pipe";
+import { SortPipe } from "./sort.pipe";
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe, SortPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
